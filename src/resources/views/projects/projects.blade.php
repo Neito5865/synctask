a4aa4fb2da4b387222a7a7c335406c088356a7bb
NeitoKomiyama <neito.komi11@gmail.com>
2024-11-05 16:16:35 +0900

    トップページ作成

--- src/resources/views/projects/projects.blade.php
+++ src/resources/views/projects/projects.blade.php
@@ -1,11 +1,66 @@
 <div class="projects__container">
     <div class="project-list">
         <ul class="project-list__container">
-            <li class="project-list__item"><a href="">テストプロジェクト</a></li>
-            <li class="project-list__item"><a href="">テストプロジェクト</a></li>
-            <li class="project-list__item"><a href="">テストプロジェクト</a></li>
-            <li class="project-list__item"><a href="">テストプロジェクト</a></li>
-            <li class="project-list__item"><a href="">テストプロジェクト</a></li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
+            <li class="project-list__item">
+                <div class="project-list__item--f-inner">
+                    <div class="project-list__item--link">
+                        <a href="">テストプロジェクト</a>
+                    </div>
+                    <div class="project-list__item--status">
+                        <p>参加中</p>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
 </div>
