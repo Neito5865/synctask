c2e733820c087112e4b530376f5f159eabf78c62
NeitoKomiyama <neito.komi11@gmail.com>
2024-11-06 16:40:38 +0900

    タスク一覧ページ・トップページ修正

--- src/resources/views/projects/projects.blade.php
+++ src/resources/views/projects/projects.blade.php
@@ -4,60 +4,60 @@
     </div>
     <div class="project-list">
         <ul class="project-list__container">
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
                     </div>
                 </div>
             </li>
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
                     </div>
                 </div>
             </li>
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
                     </div>
                 </div>
             </li>
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
                     </div>
                 </div>
             </li>
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
                     </div>
                 </div>
             </li>
-            <li class="project-list__item">
+            <li class="project-list__item" onclick="location.href='{{ route('project.index') }}'">
                 <div class="project-list__item--f-inner">
-                    <div class="project-list__item--link">
-                        <a href="">テストプロジェクト</a>
+                    <div class="project-list__item--projectName">
+                        <p>テストプロジェクト</p>
                     </div>
                     <div class="project-list__item--status">
                         <p>参加中</p>
--- src/resources/views/tasks/tasks.blade.php
+++ src/resources/views/tasks/tasks.blade.php
@@ -4,56 +4,60 @@
     </div>
     <div class="tasks-table">
         <table class="tasks-table__inner">
-            <tr class="tasks-table__row">
-                <th class="tasks-table__heading">タスク名</th>
-                <th class="tasks-table__heading">担当者</th>
-                <th class="tasks-table__heading">状態</th>
-                <th class="tasks-table__heading">期限日</th>
-                <th class="tasks-table__heading">登録日</th>
-            </tr>
-            <tr class="tasks-table__row">
-                <td class="tasks-table__item">テストタスク</td>
-                <td class="tasks-table__item">テスト太郎</td>
-                <td class="tasks-table__item">未対応</td>
-                <td class="tasks-table__item">2024/11/10</td>
-                <td class="tasks-table__item">2024/10/10</td>
-            </tr>
-            <tr class="tasks-table__row">
-                <a href="#">
+            <thead>
+                <tr class="tasks-table__row">
+                    <th class="tasks-table__heading">タスク名</th>
+                    <th class="tasks-table__heading">担当者</th>
+                    <th class="tasks-table__heading">状態</th>
+                    <th class="tasks-table__heading">期限日</th>
+                    <th class="tasks-table__heading">登録日</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="tasks-table__row" onclick="location.href='{{ route('index') }}'">
                     <td class="tasks-table__item">テストタスク</td>
                     <td class="tasks-table__item">テスト太郎</td>
                     <td class="tasks-table__item">未対応</td>
                     <td class="tasks-table__item">2024/11/10</td>
                     <td class="tasks-table__item">2024/10/10</td>
-                </a>
-            </tr>
-            <tr class="tasks-table__row">
-                <a href="#">
+                </tr>
+            </tbody>
+            <tbody>
+                <tr class="tasks-table__row" onclick="location.href='{{ route('index') }}'">
                     <td class="tasks-table__item">テストタスク</td>
                     <td class="tasks-table__item">テスト太郎</td>
                     <td class="tasks-table__item">未対応</td>
                     <td class="tasks-table__item">2024/11/10</td>
                     <td class="tasks-table__item">2024/10/10</td>
-                </a>
-            </tr>
-            <tr class="tasks-table__row">
-                <a href="#">
+                </tr>
+            </tbody>
+            <tbody>
+                <tr class="tasks-table__row" onclick="location.href='{{ route('index') }}'">
                     <td class="tasks-table__item">テストタスク</td>
                     <td class="tasks-table__item">テスト太郎</td>
                     <td class="tasks-table__item">未対応</td>
                     <td class="tasks-table__item">2024/11/10</td>
                     <td class="tasks-table__item">2024/10/10</td>
-                </a>
-            </tr>
-            <tr class="tasks-table__row">
-                <a href="#">
+                </tr>
+            </tbody>
+            <tbody>
+                <tr class="tasks-table__row" onclick="location.href='{{ route('index') }}'">
                     <td class="tasks-table__item">テストタスク</td>
                     <td class="tasks-table__item">テスト太郎</td>
                     <td class="tasks-table__item">未対応</td>
                     <td class="tasks-table__item">2024/11/10</td>
                     <td class="tasks-table__item">2024/10/10</td>
-                </a>
-            </tr>
+                </tr>
+            </tbody>
+            <tbody>
+                <tr class="tasks-table__row" onclick="location.href='{{ route('index') }}'">
+                    <td class="tasks-table__item">テストタスク</td>
+                    <td class="tasks-table__item">テスト太郎</td>
+                    <td class="tasks-table__item">未対応</td>
+                    <td class="tasks-table__item">2024/11/10</td>
+                    <td class="tasks-table__item">2024/10/10</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 </div>
