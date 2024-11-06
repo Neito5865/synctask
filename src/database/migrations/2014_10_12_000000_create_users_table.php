b8b7accbaf4c54297b64113397d7fae0779d65d7
NeitoKomiyama <neito.komi11@gmail.com>
2024-11-06 20:34:24 +0900

    usersテーブル、projectsテーブル作成とマイグレーション

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 80);
             $table->string('email')->unique();
+            $table->string('tel', 15);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
